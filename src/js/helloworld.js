15b7832a600b9ecef950311ec389e98e2f7433f9
heqifang <598910792@qq.com>
2017-02-15 14:06:53 +0800

    Create helloworld.js

---
+++ src/js/helloworld.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2017/1/6.
+ */
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, world.let us  go </h1>
+                <p>终于搭建成功了 </p>
+                <p>为什么远程引用不到热加载js啊啊，什么破玩意</p>
+                <p>我有一句mmp不知当讲不当讲无语</p>
+                <p>heqi</p>
+            </div>
+
+        );
+    }
+}
